7784a43328c37356328b98eff6b0401c28c6fdd8
NadaFayez13 <naditafayez13@gmail.com>
2026-09-15 19:52:16 +0300

    sheet5_question4

--- oop_1/Program.cs
+++ oop_1/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace oop_1
+﻿using System.Drawing;
+
+namespace oop_1
 {
+    public enum Genre
+    {
+        Fiction,
+        NonFiction,
+        Science
+    }
+    public class Book
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //sheet5
+            #region question 4
+            Book book = new Book();
+            book.Title = "Science book";
+            book.Genre = Genre.Science;
+            Console.WriteLine($"Title: {book.Title}");
+            Console.WriteLine($"Genre: {book.Genre}");
+            #endregion
+
+
+
         }
     }
 }
