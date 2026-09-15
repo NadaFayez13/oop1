4bede5926950500c34efcf1ab27dd6030fcf887f
NadaFayez13 <naditafayez13@gmail.com>
2026-09-15 20:28:49 +0300

    sheet6 Theoretical Question 1

--- oop_1/Program.cs
+++ oop_1/Program.cs
@@ -27,47 +27,60 @@ namespace oop_1
             Console.WriteLine($"Genre: {book.Genre}");
             #endregion
 
-            #region question 5
-            /*Using the Genre enum above, print the underlying int value Genre.Fiction,
-           Genre.NonFiction, and Genre.Science by casting each to int.*/
-            //int fictionValue = (int)Genre.Fiction;
-            //int nonFictionValue = (int)Genre.NonFiction;
-            //int scienceValue = (int)Genre.Science;
+#region question 5
+/*Using the Genre enum above, print the underlying int value Genre.Fiction,
+Genre.NonFiction, and Genre.Science by casting each to int.*/
+//int fictionValue = (int)Genre.Fiction;
+//int nonFictionValue = (int)Genre.NonFiction;
+//int scienceValue = (int)Genre.Science;
 
-            //Console.WriteLine($"Genre.Fiction: {fictionValue}");
-            //Console.WriteLine($"Genre.NonFiction: {nonFictionValue}");
-            //Console.WriteLine($"Genre.Science: {scienceValue}");
+//Console.WriteLine($"Genre.Fiction: {fictionValue}");
+//Console.WriteLine($"Genre.NonFiction: {nonFictionValue}");
+//Console.WriteLine($"Genre.Science: {scienceValue}");
 
-            #endregion
+#endregion
 
-            #region question 6
-            // Given int genreNumber = 1;, cast it into a Genre value and print the result.
-            //int genreNumber = 1;
-            //Genre genre = (Genre)genreNumber;
-            //Console.WriteLine($"Genre: {genre}");
-            #endregion
+#region question 6
+// Given int genreNumber = 1;, cast it into a Genre value and print the result.
+//int genreNumber = 1;
+//Genre genre = (Genre)genreNumber;
+//Console.WriteLine($"Genre: {genre}");
+#endregion
 
-            #region question 7
-            /*Given string genreText = "Science";, convert it into a Genre value using
-            Enum.Parse() and print the result.*/
-            //string genreText = "Science";
-            //Genre genre = (Genre)Enum.Parse(typeof(Genre), genreText);
-            //Console.WriteLine($"genre: {genre}");
-            #endregion
+#region question 7
+/*Given string genreText = "Science";, convert it into a Genre value using
+Enum.Parse() and print the result.*/
+//string genreText = "Science";
+//Genre genre = (Genre)Enum.Parse(typeof(Genre), genreText);
+//Console.WriteLine($"genre: {genre}");
+#endregion
 
-            #region question 8
-            /*Given string genreText = "Mystery"; (not a valid Genre value), use
-          Enum.TryParse() to attempt the conversion. Print "Unknown genre" if it fails.*/
-            string genreText = "Mystery";
-            if (Enum.TryParse(genreText, out Genre genre))
-            {
-                Console.WriteLine($"genre: {genre}");
-            }
-            else
-            {
-                Console.WriteLine("Unknown genre");
-            }
-            #endregion
-        }
-    }
+#region question 8
+/*Given string genreText = "Mystery"; (not a valid Genre value), use
+Enum.TryParse() to attempt the conversion. Print "Unknown genre" if it fails.*/
+//string genreText = "Mystery";
+//if (Enum.TryParse(genreText, out Genre genre))
+//{
+//    Console.WriteLine($"genre: {genre}");
+//}
+//else
+//{
+//    Console.WriteLine("Unknown genre");
+//}
+#endregion
+
+//sheet6
+#region Theoretical Question 1
+//a) What happens when a DeliveryAddress variable is copied into another variable and the copy is modified?
+
+/*Since DeliveryAddress is a struct, copying it to another variable creates an independent copy of the data on the stack.
+  Therefore, any changes made to the new variable will not affect the original one at all, 
+ as they point to two separate memory locations.*/
+
+//b) What happens when a Customer variable is copied into another variable and one variable modifies the object
+/*Since Customer is a class, copying the variable copies only its memory reference, so both variables point to the same object on the heap. 
+ * Modifying the object through one variable will affect the other.*/
+#endregion
+}
+}
 }
